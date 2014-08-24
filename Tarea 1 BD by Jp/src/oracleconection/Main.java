ae868f42c99f16e547c68e7ad222bce67849c89a
JuanPabloCastillo <jpcv12@hotmail.com>
2014-08-24 18:49:40 -0400

    Avance de Jp
    Esto es lo que hize el domingo 24 de agosto

---
+++ Tarea 1 BD by Jp/src/oracleconection/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oracleconection;
+
+import java.io.*;
+import java.sql.*;
+import javax.swing.*;
+
+public class Main {
+
+    static Connection conn=null;
+    static Statement st=null;
+    static ResultSet rs=null;
+    
+ 
+    
+    static String bd="XE";
+    static String login="TAREA1BD";
+    static String password="marti";
+    static String url="jdbc:oracle:thin:@localhost:1521:XE";
+    
+    public static Connection Enlace(Connection conn)throws SQLException    {
+        try {
+         Class.forName("oracle.jdbc.OracleDriver");
+         conn=DriverManager.getConnection(url, login, password);
+        }
+        catch(ClassNotFoundException e )
+        {
+            System.out.print("Clase no encontrada");
+        }
+        return conn;
+    }
+    
+    public static Statement sta(Statement st)throws SQLException    {
+        conn=Enlace(conn);
+        st=conn.createStatement();
+        return st;
+    }
+    public static ResultSet EnlEst(ResultSet rs)throws SQLException    {
+       st=sta(st);
+       rs=st.executeQuery("select * from usuario");
+        return rs;
+    }
+
+}
